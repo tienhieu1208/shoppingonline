8b880026809e2d9df9a794dd3aba2f6a7fc58d73
tienhieu1208 <hieu.2174802010722@vanlanguni.vn>
2023-11-07 11:28:17 +0700

    project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.bcsaenn.mongodb.net',
+    DB_USER: 'hieu',
+    DB_PASS: 'Hieu1208',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'wibu',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'tienhieu1208@gmail.com', // gmail service
+    EMAIL_PASS: 'axqsqozqgaszwcuu'
+  };
+  module.exports = MyConstants;
